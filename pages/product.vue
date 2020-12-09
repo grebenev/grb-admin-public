b31e87fd51ba2cd864e34f1bad0912a94d544e63
Konstantin <konstantin@grebenev.com>
2020-12-09 10:03:51 +0300

    fix: big change of form creation structure, selects added

--- pages/product.vue
+++ pages/product.vue
@@ -6,10 +6,16 @@
       <section>
         <Form
           @onSubmit="onSubmit($event)"
-          :config="formConfig"
+          :config="formConfiguration"
           formName="Add new product"
         />
       </section>
+
+      <div v-if="messages">
+        <div v-for="(message, index) in messages" :key="index">
+          MESSAGE: {{ message.message }}
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -19,6 +25,14 @@ import Form, { FormConfig } from '@/components/Form.vue';
 import ButtonUi from '@/components/UI/ButtonUi.vue';
 
 import { Component, Vue } from 'nuxt-property-decorator';
+import { Context } from '@nuxt/types';
+import { AxiosResponse, AxiosPromise, AxiosError } from 'axios';
+
+interface Select {
+  type: string;
+  name: string;
+  _id: string;
+}
 
 @Component({
   components: {
@@ -28,35 +42,95 @@ import { Component, Vue } from 'nuxt-property-decorator';
 })
 export default class Product extends Vue {
   header: string = 'Product page';
+  messages: { message: string }[] = [];
 
-  formConfig: FormConfig = {
-    inputs: {
-      title: { name: 'Заголовок', value: '', placeholder: 'place title' },
-      description: { value: '', control: 'textarea' },
-      price: { value: '', placeholder: 'place price' },
-    },
-    selects: {
-      owners: {
-        options: [
-          { name: 'Choose one of the owners', value: '' },
-          { name: 'owner1', value: 'owner1' },
-          { name: 'owner2', value: 'owner2' },
-        ],
-        value: '',
-      },
-      categories: {
-        options: [
-          { name: 'Choose one of the categories', value: '' },
-          { name: 'category1', value: 'category1' },
-          { name: 'category2', value: 'category2', selected: 'category2' },
-        ],
-        value: '',
-      },
-    },
-  };
+  async onSubmit(formData: {}): Promise<void> {
+    console.log(formData);
 
-  onSubmit(formData: {}): void {
-    console.log('Emmit data from Form: ', formData);
+    await this.$axios
+      .post('http://localhost:3000/api/products/product', formData)
+
+      .then((res: AxiosResponse) => {
+        this.messages = res.data.success;
+
+        // this.$router.push('/');
+      })
+      .catch((err: AxiosError) => {
+        this.messages = err.response?.data.errors;
+      });
+  }
+
+  asyncData({ $axios, error }: Context) {
+    return Promise.all([
+      $axios.get(`http://localhost:3000/api/categories`),
+      $axios.get(`http://localhost:3000/api/owners`),
+    ])
+      .then(([categoriesRes, ownersRes]) => {
+        const { categories } = categoriesRes.data;
+        const { owners } = ownersRes.data;
+
+        const formatSelects = (selects: Select[]): object[] => {
+          let formatted: object[] = [];
+
+          formatted = selects.map((select: Select): {} => {
+            return {
+              name: select.type ? select.type : select.name,
+              value: select._id,
+            };
+          });
+
+          return formatted;
+        };
+
+        return {
+          formConfiguration: {
+            inputs: {
+              title: {
+                name: 'Заголовок',
+                value: '',
+                placeholder: 'product title',
+              },
+              description: {
+                value: '',
+                control: 'textarea',
+                placeholder: 'product description',
+              },
+              price: {
+                value: '',
+                placeholder: '0',
+                type: 'number',
+              },
+              stockOuantity: {
+                value: '',
+                placeholder: '0',
+                type: 'number',
+              },
+            },
+            selects: {
+              owner: {
+                options: [
+                  { name: 'Choose one owner', value: '' },
+                  ...formatSelects(owners),
+                ],
+                value: '',
+              },
+              category: {
+                options: [
+                  { name: 'Choose one category', value: '' },
+                  ...formatSelects(categories),
+                ],
+                value: '',
+              },
+            },
+          } as FormConfig,
+        };
+      })
+      .catch((err) => {
+        error({
+          statusCode: 404,
+          message: `No items found - ${err.message}`,
+        });
+      });
   }
 }
 </script>
