db270273f7d6aae157680c7d252a51e69383ac1d
Konstantin <konstantin@grebenev.com>
2020-12-10 12:33:48 +0300

    new page added

---
+++ pages/category.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <div class="container">
+      <h1>{{ header }}</h1>
+
+      <section>
+        <Form
+          @onSubmit="onSubmit($event)"
+          :config="formConfiguration"
+          formName="Add new category"
+        />
+      </section>
+
+      <div>
+        <div>MESSAGE:</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+import { FormConfig } from '@/components/Form.vue';
+
+@Component
+export default class Category extends Vue {
+  header: string = 'Category page';
+
+  formConfiguration: FormConfig = {
+    inputs: {
+      category: {
+        name: 'Create a new caterory',
+        value: '',
+        placeholder: 'place a category',
+      },
+    },
+  };
+
+  onSubmit(formData: {}): void {
+    console.log(formData);
+  }
+}
+</script>
