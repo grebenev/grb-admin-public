a9db109c8489e0523550c49a4e876c5e80f776f1
Konstantin <konstantin@grebenev.com>
2021-01-13 10:07:29 +0300

    feature: delete product added

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="card">
+  <article class="card" :class="{ 'card--deleted': getProduct.deleted }">
     <div class="card__wrapper">
       <div class="card__item card__item--title">
         <h2 class="title">{{ getProduct.title }}</h2>
@@ -25,8 +25,22 @@
       </div>
 
       <div class="card__item card__item--tools">
-        <ButtonUi>Edit product</ButtonUi>
-        <ButtonUi>Delete product</ButtonUi>
+        <ButtonUi
+          type="button"
+          @click="$router.push(`products/${getProduct._id}`)"
+          >Edit product</ButtonUi
+        >
+        <ButtonUi
+          v-if="!getProduct.deleted"
+          type="button"
+          @click="deleteProduct(getProduct._id)"
+          >Delete product</ButtonUi
+        >
+      </div>
+      <div v-if="messages">
+        <div v-for="(message, index) in messages" :key="index">
+          MESSAGE: {{ message.message }}
+        </div>
       </div>
     </div>
   </article>
@@ -35,20 +49,25 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator';
 import { ImageConfig } from '@/components/UI/ImageUi.vue';
+import { AxiosResponse, AxiosError } from 'axios';
 
 interface CardConfig {
+  _id: string;
   title: string;
   rating: [];
   description: number;
   photos: {};
   price: number;
   stockOuantity: number;
+  deleted?: boolean;
 }
 
 @Component
 export default class Card extends Vue {
   @Prop({ type: Object, required: true })
   readonly product!: CardConfig;
+
+  messages: { message: string }[] = [];
 
   imageConfig: ImageConfig = {
     desktop: { w: 300, h: 300 },
@@ -59,5 +78,23 @@ export default class Card extends Vue {
   get getProduct() {
     return this.product;
   }
+
+  async deleteProduct(id: string): Promise<void> {
+    await this.$axios
+      .put(`http://localhost:3000/api/products/delete/${id}`)
+      .then((res: AxiosResponse) => {
+        this.messages = res.data.success;
+      })
+      .catch((err: AxiosError) => {
+        this.messages = err.response ? err.response.data.errors : [];
+      });
+  }
 }
 </script>
+<style lang="scss">
+.card {
+  &--deleted {
+    opacity: 0.3;
+  }
+}
+</style>
